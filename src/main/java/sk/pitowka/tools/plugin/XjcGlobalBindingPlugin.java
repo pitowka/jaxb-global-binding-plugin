ebbe23ea8beb90d33d5fbbb3b70827ce46d6ec50
pitowka <pitowka@gmail.com>
2020-09-21 18:31:48 +0200

    write better help text

--- src/main/java/sk/pitowka/tools/plugin/XjcGlobalBindingPlugin.java
+++ src/main/java/sk/pitowka/tools/plugin/XjcGlobalBindingPlugin.java
@@ -14,7 +14,7 @@ public class XjcGlobalBindingPlugin extends Plugin{
 
 	@Override
 	public String getOptionName() {
-		return "Xglobal-binding";
+		return "Xgb";
 	}
 	
 	
@@ -28,33 +28,39 @@ public class XjcGlobalBindingPlugin extends Plugin{
 	public String getUsage() {
 		return new StringBuilder()
 			.append("Usage: -Xgb").append("\n").append("\n")
-			.append("Enable some of global binding features").append("\n").append("\n")
+			.append("Enable some of global binding features as cli options.").append("\n").append("\n")
 			.append("Options:").append("\n").append("\n")
 			
 			.append("\t").append("-simple").append("\n")
-			.append("\t").append("\t").append("<xjc:simple/>").append("\n").append("\n")
+			.append("\t").append("\t").append("add  <xjc:simple/>").append("\n").append("\n")
 			
 			.append("\t").append("-serializable").append("\n")
-			.append("\t").append("\t").append("<xjc:serializable/>").append("\n").append("\n")
+			.append("\t").append("\t").append("add <xjc:serializable/>").append("\n").append("\n")
 			
 			.append("\t").append("-no-element-property").append("\n")
-			.append("\t").append("\t").append("<xjc:generateElementProperty>false</xjc:generateElementProperty>").append("\n").append("\n")
+			.append("\t").append("\t").append("add <xjc:generateElementProperty>false</xjc:generateElementProperty>").append("\n").append("\n")
 			
-			.append("\t").append("-date-type").append("\n")
-			.append("\t").append("\t").append("xjc:javaType name=value").append("\n")
-			.append("\t").append("-date-adapter").append("\n")
-			.append("\t").append("\t").append("xjc:javaType adapter=value").append("\n").append("\n")
+			.append("\t").append("-date-type dateType").append("\n")
+			.append("\t").append("\t").append("for xjc:javaType xmlType=\"xs:date\" set name=dateType").append("\n")
+			.append("\t").append("-date-adapter dateTypeAdapter").append("\n")
+			.append("\t").append("\t").append("for xjc:javaType xmlType=\"xs:date\" set adapter=dateTypeAdapter").append("\n").append("\n")
 			
-			.append("\t").append("-dateTime-type").append("\n")
-			.append("\t").append("\t").append("xjc:javaType name=value").append("\n")
-			.append("\t").append("-dateTime-adapter").append("\n")
-			.append("\t").append("\t").append("xjc:javaType adapter=value").append("\n").append("\n")
+			.append("\t").append("-dateTime-type dateTimeType").append("\n")
+			.append("\t").append("\t").append("for xjc:javaType xmlType=\"xs:dateTime\" set name=dateTimeType").append("\n")
+			.append("\t").append("-dateTime-adapter dateTimeAdapter").append("\n")
+			.append("\t").append("\t").append("for xjc:javaType xmlType=\"xs:dateTime\" set adapter=dateTimeAdapter").append("\n").append("\n")
 			
-			.append("\t").append("-time-type").append("\n")
-			.append("\t").append("\t").append("xjc:javaType name=value").append("\n")
-			.append("\t").append("-time-adapter").append("\n")
-			.append("\t").append("\t").append("xjc:javaType adapter=value").append("\n").append("\n")
+			.append("\t").append("-time-type timeType").append("\n")
+			.append("\t").append("\t").append("for xjc:javaType xmlType=\"xs:time\" set name=timeType").append("\n")
+			.append("\t").append("-time-adapter timeAdapter").append("\n")
+			.append("\t").append("\t").append("for xjc:javaType xmlType=\"xs:time\" set adapter=timeAdapter").append("\n").append("\n")
 			
+			.append("E.g.:").append("\t").append("-Xgb\\").append("\n\t")
+			.append("-simple\\").append("\n\t")
+			.append("-serializable\\").append("\n\t")
+			.append("-date-type java.time.LocalDate -date-adapter package.XmlDateAdapter\\").append("\n\t")
+			.append("-time-type java.time.LocalTime -time-adapter package.XmlTimeAdapter\\").append("\n\t")
+			.append("-dateTime-type java.time.LocalDateTime -dateTime-adapter package.XmlDateTimeAdapter").append("\n").append("\n")
 			
 			.toString();
 	}
